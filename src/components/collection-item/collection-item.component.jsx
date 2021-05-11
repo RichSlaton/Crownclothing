83bc9c3f18127a057525b6d90a56b68108e4de1d
darby <darbyslaton@gmail.com>
2021-05-11 19:57:20 -0400

    Finish Redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
+
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
+  <div className="collection-item">
     <div
-      className='image'
+      className="image"
       style={{
-        backgroundImage: `url(${imageUrl})`
+        backgroundImage: `url(${imageUrl})`,
       }}
     />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 );
 
