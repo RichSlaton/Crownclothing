8e8719d0a069552d6752470c648af06c32c089b8
darby <darbyslaton@gmail.com>
2021-05-11 09:18:16 -0400

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
+  <Provider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
